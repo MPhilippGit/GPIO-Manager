20847a404eefe13d6915f1fe0dc766627911b120
Vendrick <philipp.seb.mueller@outlook.de>
2025-11-08 20:33:47 +0100

    additional views for fetch endpoints, template for base dashboard

--- energymanager/GPIO/urls.py
+++ energymanager/GPIO/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("api/temps", views.fetch_temperatures, name="temps")
+    path("api/temps", views.fetch_temperatures, name="temps"),
+    path("api/humids", views.fetch_humidities, name="humids"),
+    path("api/vcos", views.fetch_vcos, name="vcos")
 ]
--- energymanager/GPIO/views.py
+++ energymanager/GPIO/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.template import loader
+from django.http import HttpResponse, JsonResponse, Http404
 from .models import TemperatureValues
 
 def index(request):
-   return HttpResponse("Blub")
+   template = loader.get_template("GPIO/dashboard.html")
+   return HttpResponse(template.render({}, request))
 
 def fetch_temperatures(request):
    data = list(TemperatureValues.objects.values("measurement", "date"))
+   return JsonResponse(data, safe=False)
+
+def fetch_humidities(request):
+   data = ["humidities", "placeholder", "value"]
+   return JsonResponse(data, safe=False)
+
+def fetch_vcos(request):
+   data = ["vco", "placeholder", "values"]
    return JsonResponse(data, safe=False)
