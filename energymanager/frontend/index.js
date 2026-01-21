d3e657d318ef4da86360d061bfd688d3239ffa7a
Philipp Mueller <philipp.mueller@rheinschafe.de>
2026-01-21 22:58:57 +0100

    fix: linting

--- energymanager/frontend/index.js
+++ energymanager/frontend/index.js
@@ -7,17 +7,17 @@ import { Chart } from 'chart.js/auto';
 console.log('Vite läuft ✅');
 
 async function fetchValuesFromSensors(endpoint) {
-   const url = endpoint;
-   try {
-      const response = await fetch(url);
+  const url = endpoint;
+  try {
+    const response = await fetch(url);
 
-     if (!response.ok) {
-       throw new Error(`Response status: ${response.status}`);
-     }
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
 
-      const result = await response.json();
-      return result;
-   }  catch (error) {
+    const result = await response.json();
+    return result;
+  } catch (error) {
     console.error(error.message);
     return null;
   }
