e60640ea2ca02266b6c56dfbabfceaedbeaaabd0
Philipp Mueller <philipp.mueller@rheinschafe.de>
2026-02-02 23:54:38 +0100

    task: vite things

--- energymanager/vite.config.js
+++ energymanager/vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
 import path from "path";
 
 export default defineConfig({
+   base: "/static/",
+   plugins: [react()],
    root: ".", // kein eigenes Root, da du index.html nicht nutzt
    build: {
-      outDir: "static/dist",
+      manifest: "manifest.json",
+      outDir: path.resolve(__dirname, './static'),
       emptyOutDir: true,
       rollupOptions: {
          input: path.resolve(__dirname, "frontend/index.js"),
          output: {
-            entryFileNames: "assets/[name].js",
+            entryFileNames: "assets/[name]-bundle.js",
             assetFileNames: "assets/[name].[ext]",
          },
       },
