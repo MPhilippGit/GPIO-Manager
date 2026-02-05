ff560eb264eb65c48288d4b26cb0f4933082d7c0
Philipp Mueller <philipp.mueller@rheinschafe.de>
2026-02-05 22:01:01 +0100

    task: logging view

--- energymanager/GPIO/views.py
+++ energymanager/GPIO/views.py
@@ -1,3 +1,4 @@
+from energymanager.settings import BASE_DIR
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse, JsonResponse, Http404
@@ -19,3 +20,13 @@ def fetch_humidities(request):
 def fetch_vcos(request):
    data = ["vco", "placeholder", "values"]
    return JsonResponse(data, safe=False)
+
+def fetch_vcos(request):
+   data = ["vco", "placeholder", "values"]
+   return JsonResponse(data, safe=False)
+
+def fetch_log(request):
+   logfile = BASE_DIR / "app.log"
+   with logfile.open("r") as f:
+      filecontent = f.readlines()
+   return HttpResponse(content=filecontent)
