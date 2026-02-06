74417054e5f129e022dadec017e22981cf8f204e
Philipp Mueller <philipp.mueller@rheinschafe.de>
2026-02-06 18:14:58 +0100

    task: added voc model

--- energymanager/GPIO/models.py
+++ energymanager/GPIO/models.py
@@ -11,3 +11,8 @@ class Humidities(models.Model):
    unit = models.CharField(max_length=50, db_column="unit")
    timestamp = models.DateTimeField("timestamp", db_column="timestamp")
    measurement =  models.DecimalField(max_digits=5, decimal_places=2, db_column="measurement")
+
+class VOCs(models.Model):
+   unit = models.CharField(max_length=50, db_column="unit")
+   timestamp = models.DateTimeField("timestamp", db_column="timestamp")
+   measurement =  models.DecimalField(max_digits=5, decimal_places=2, db_column="measurement")
