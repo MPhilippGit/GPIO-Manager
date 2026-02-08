e58b4631d694c68b0d4da5dc398574f5e70457a3
Philipp Mueller <philipp.mueller@rheinschafe.de>
2026-02-08 22:04:34 +0100

    task: simplified chart

--- energymanager/frontend/components/Board.jsx
+++ energymanager/frontend/components/Board.jsx
@@ -1,51 +1,26 @@
-import { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
 import "../scss/components/board.scss";
 
 function Board() {
-    const chartRef = useRef(null);
-    let chartInstance = null;
-
-    useEffect(() => {
-        const myChartRef = chartRef.current.getContext("2d");
-        
-        const labels = ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00'];
-
-        chartInstance = new Chart(myChartRef, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [
-                    {
-                        label: "Temperature (°C)",
-                        data: [20, 21, 22, 21.5, 22, 23],
-                        fill: false,
-                        borderColor: 'rgba(255, 99, 132, 1)',
-                        tension: 0.1
-                    },
-                ]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
+    const lineData = {
+            labels: ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00'],
+            datasets: [
+                {
+                    label: "Temperature (°C)",
+                    data: [20, 21, 22, 21.5, 22, 23],
+                    fill: false,
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    tension: 0.1
                 },
-                responsive: true,
-                maintainAspectRatio: false
-            }
-        });
-
-        return () => {
-            chartInstance.destroy();
+            ]
         }
-    }, []);
 
     return (
         <main className="dash-main dash-container">
             <h2>Energy Monitor</h2>
             <div className='dash-main_graph'>
-                <canvas ref={chartRef} />
+                <Line data={lineData} />
             </div>
         </main>
     );
