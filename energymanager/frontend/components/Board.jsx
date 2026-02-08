4835d2e72a2763e0eea4f2c468e16927773ce320
Philipp Mueller <philipp.mueller@rheinschafe.de>
2026-02-09 00:13:05 +0100

    task: actually fetch data from database

--- energymanager/frontend/components/Board.jsx
+++ energymanager/frontend/components/Board.jsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from 'react';
 import Chart from 'chart.js/auto';
 import { Line } from 'react-chartjs-2';
 import { tempOptions } from '../utils/chart-options.js';
 import "../scss/components/board.scss";
 
+
 function Board() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetchSensorData("api/temps");
+    }, [])
+
+    const fetchSensorData = async (endpoint) => {
+        try {
+            const response = await fetch(endpoint);
+            const result = await response.json();
+            setData(result)
+        }
+        catch (error) {
+            console.error(error.message)
+            setData({})
+        }
+    }
+    const tempValues = data.map(value => value.measurement)
+    const labelValues = data.map(value => value.timestamp)
+
     const lineData = {
-            labels: ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00'],
+            labels: labelValues,
             datasets: [
                 {
                     label: "Temperature (°C)",
-                    data: [20, 21, 22, 21.5, 22, 23],
+                    data: tempValues,
                     fill: false,
                     borderColor: 'rgba(255, 99, 132, 1)',
                     tension: 0.1
