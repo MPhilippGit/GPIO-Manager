41cd81309fd1f8bf5c170fea93b3bb95a78d9560
Philipp Mueller <philipp.mueller@rheinschafe.de>
2026-02-11 23:45:15 +0100

    task: class for reading stable data from bme680

--- energymanager/GPIO/models.py
+++ energymanager/GPIO/models.py
@@ -51,3 +51,19 @@ class VOCs(models.Model):
       except Exception as error:
          # Log errors 
          logger.error(f"error while trying to write to db: {error}")
+
+class Pressures(models.Model):
+   unit = models.CharField(max_length=50, db_column="unit")
+   timestamp = models.DateTimeField("timestamp", db_column="timestamp")
+   measurement =  models.DecimalField(max_digits=5, decimal_places=2, db_column="measurement")
+
+   @staticmethod
+   def save_pressure(measurement):
+      logger = logging.getLogger(__name__)
+      try:
+         new_measurement = Pressures(measurement=measurement, unit="hPa", timestamp=timezone.now())
+         new_measurement.save()
+         logger.info(f"new pressure measurement: {measurement}")
+      except Exception as error:
+         # Log errors 
+         logger.error(f"error while trying to write to db: {error}")
---
+++ energymanager/GPIO/sensors/bme680.py
@@ -0,0 +1,61 @@
+import logging
+import time
+import bme680
+
+class BME680Data:
+    def __init__(self):
+        # initialize
+        try:
+            self.sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+        except (RuntimeError, IOError):
+            self.sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+        self.sensor.set_humidity_oversample(bme680.OS_2X)
+        self.sensor.set_pressure_oversample(bme680.OS_4X)
+        self.sensor.set_temperature_oversample(bme680.OS_8X)
+        self.sensor.set_filter(bme680.FILTER_SIZE_3)
+        self.sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+
+    def data_dump(self):
+        """returns a dictionary for every possible measurement from the sensor"""
+        data_dict = {}
+        for name in dir(self.sensor.data):
+            value = getattr(self.sensor.data, name)
+            if not name.startswith('_'):
+                data_dict[name] = value
+        return data_dict
+
+    def get_read(self):
+        """Trys to fetch data when heat is stable for gas measurement. Warning log if heat can't be stabilized"""
+        i = 0
+        while i < 10:
+            if not self.sensor.data.heat_stable:
+                i+=1
+                time.sleep(1)
+                continue
+            return self.data_dump()
+        BME680Data.log_unstable_read()
+        return self.data_dump()
+
+    @staticmethod
+    def log_unstable_read():
+        logger = logging.getLogger(__name__)
+        logger.warning("No stable VOC read")
+
+
+
+# sampling settings
+
+# for name in dir(sensor.data):
+#     value = getattr(sensor.data, name)
+
+#     if not name.startswith('_'):
+#         print('{}: {}'.format(name, value))
+
+# sensor.set_gas_heater_temperature(320)
+# sensor.set_gas_heater_duration(150)
+# sensor.select_gas_heater_profile(0)
+
+# # Up to 10 heater profiles can be configured, each
+# # with their own temperature and duration.
+# # sensor.set_gas_heater_profile(200, 150, nb_profile=1)
+# # sensor.select_gas_heater_profile(1)
