f17750f1526bb065e59b9ee08806d657ec4842e4
Philipp Mueller <philipp.mueller@rheinschafe.de>
2026-02-13 01:00:01 +0100

    task: updated data modeling and regression data

--- GPIO/models.py
+++ GPIO/models.py
@@ -4,9 +4,21 @@ import logging
 
 # Create your models here.
 
-class Temperatures(models.Model):
+class Measurements(models.Model):
    unit = models.CharField(max_length=50, db_column="unit")
    timestamp = models.DateTimeField(db_column="timestamp")
+
+   class Meta:
+      abstract = True
+
+   @classmethod
+   def cleanup_entries(cls, timespan=30):
+      cutoff = timezone.now() - timezone.timedelta(minutes=60)
+      result = cls.objects.filter(timestamp__gt=cutoff)
+      deleted, _ = result.delete()
+      return deleted
+
+class Temperatures(Measurements):
    measurement =  models.DecimalField(max_digits=5, decimal_places=2, db_column="measurement")
 
    @staticmethod
@@ -20,9 +32,7 @@ class Temperatures(models.Model):
          # Log errors 
          logger.error(f"error while trying to write to db: {error}")
 
-class Humidities(models.Model):
-   unit = models.CharField(max_length=50, db_column="unit")
-   timestamp = models.DateTimeField("timestamp", db_column="timestamp")
+class Humidities(Measurements):
    measurement =  models.DecimalField(max_digits=5, decimal_places=2, db_column="measurement")
 
    @staticmethod
@@ -36,9 +46,7 @@ class Humidities(models.Model):
          # Log errors 
          logger.error(f"error while trying to write to db: {error}")
 
-class VOCs(models.Model):
-   unit = models.CharField(max_length=50, db_column="unit")
-   timestamp = models.DateTimeField("timestamp", db_column="timestamp")
+class VOCs(Measurements):
    measurement =  models.DecimalField(max_digits=8, decimal_places=2, db_column="measurement")
 
    @staticmethod
@@ -52,9 +60,7 @@ class VOCs(models.Model):
          # Log errors 
          logger.error(f"error while trying to write to db: {error}")
 
-class Pressures(models.Model):
-   unit = models.CharField(max_length=50, db_column="unit")
-   timestamp = models.DateTimeField("timestamp", db_column="timestamp")
+class Pressures(Measurements):
    measurement =  models.DecimalField(max_digits=5, decimal_places=2, db_column="measurement")
 
    @staticmethod
